444f5a7caa7b665f1e460593a30eca49f4131da9
Luis Rodrigues <itslhuis.dev@gmail.com>
2025-02-27 11:27:22 +0000

    Add utils

---
+++ src/utils/format.ts
@@ -0,0 +1,17 @@
+import { format, isToday, isYesterday, differenceInHours, parseISO } from "date-fns"
+
+export function formatRelativeDate(data: Date | string): string {
+  const date = typeof data === "string" ? parseISO(data) : data
+
+  const diffHoras = differenceInHours(new Date(), date)
+
+  if (diffHoras < 24) {
+    if (diffHoras < 1) return "Less than an hour ago"
+    return `${diffHoras} ${diffHoras === 1 ? "hour" : "hours"} ago`
+  }
+
+  if (isToday(date)) return "Today"
+  if (isYesterday(date)) return "Yesterday"
+
+  return format(date, "d MMMM, yyyy")
+}
